cbb98f232292cc846f8721280dbd077111f26830
mofetaiwo <87393730+mofetaiwo@users.noreply.github.com>
2022-08-31 14:31:32 -0400

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+package edu.pitt.mot39_helloworld;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("HELLO WORLD");
+	}
+
+}
