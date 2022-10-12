a92c18b92046774cc856715fca918f8a1f822fa2
mofetaiwo <87393730+mofetaiwo@users.noreply.github.com>
2022-10-11 22:58:43 -0400

    Add files via upload

---
+++ assignment1/Side.java
@@ -0,0 +1,64 @@
+package edu.pitt.menumanager;
+
+/**
+ * Class Side
+ * @author mofetaiwo
+ * Created: 10/03/2022
+ */
+public class Side {
+	private String name;
+	private String description;
+	private int calories;
+	
+	//constructor
+	
+	public Side (String name, String desc, int cal){
+		this.name = name;
+		this.description = desc;
+		this.calories = cal;
+	}
+	
+	//getters and setters
+	
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param set name to the name
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the description
+	 */
+	public String getDescription() {
+		return description;
+	}
+
+	/**
+	 * @param set description to the description
+	 */
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	/**
+	 * @return the calories
+	 */
+	public int getCalories() {
+		return calories;
+	}
+
+	/**
+	 * @param set calories to the calories
+	 */
+	public void setCalories(int calories) {
+		this.calories = calories;
+	}
+}
